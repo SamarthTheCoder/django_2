73e590d3f66bdf1be013f15b32118e9e28f11738
SamarthTheCoder <samarthbagga@gmail.com>
2021-10-09 16:30:20 +0530

    I have made this change

---
+++ learning_templates/basic_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'basic_app/index.html')
+def other(request):
+    return render(request,'basic_app/other.html')
+
+def relative(request):
+    return render(request,'basic_app/relative_url_templates.html')
+
+def index(request):
+    context_dict={'text':'hello world','number':100}
+    return render(request,'basic_app/index.html',context_dict)
+    
+
+# Create your views here.
